bae6815d5b2b8944641639e89a61951bc42486f0
Negar <negarkazemi38@gmail.com>
2025-04-30 11:46:22 +0330

    Fixing bugs.

--- src/products/Accessory.java
+++ src/products/Accessory.java
@@ -1,6 +1,6 @@
 package products;
 
-public class Accessory extends Product{
+public class Accessory extends Product {
     private String color;
     protected static int idCounter = 1;
 
@@ -16,22 +16,26 @@ public class Accessory extends Product{
     public void setColor(String color) {
         this.color = color;
     }
+
     @Override
-    public String toString(){
+    public String toString() {
         return super.toString() +
                 "   Color: " + getColor();
 
     }
 
     @Override
-    protected String generateId(){
+    protected String generateId() {
         String formattedId;
         if (idCounter < 10) {
             formattedId = "300" + idCounter;
         } else if (idCounter < 100) {
             formattedId = "30" + idCounter;
-        } else {
+        } else if (idCounter < 1000) {
             formattedId = "3" + idCounter;
+        } else {
+            System.out.println("ID limit reached");
+            return null;
         }
         idCounter++;
         return formattedId;
